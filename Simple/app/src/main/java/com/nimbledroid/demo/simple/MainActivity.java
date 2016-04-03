916c64d8193c69b6c5a84942da54a0d638db1f7d
Stéphane Nicolas <steff.nicolas@gmail.com>
2016-04-02 21:12:52 -0700

    Add version without DI

---
+++ Simple/app/src/main/java/com/nimbledroid/demo/simple/MainActivity.java
@@ -0,0 +1,14 @@
+package com.nimbledroid.demo.simple;
+
+import android.app.Activity;
+import android.os.Bundle;
+import com.demo.simple.R;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
